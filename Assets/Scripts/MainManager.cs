1f6ad90e23e885d3eb3f05dc3cbcf7144b8299d1
arousseau00 <77174802+arousseau00@users.noreply.github.com>
2023-02-21 15:13:23 -0500

    Update MainManager.cs

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,24 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+    public static MainManager Instance;
+
     public Brick BrickPrefab;
     public int LineCount = 6;
+    public int score;
     public Rigidbody Ball;
-
     public Text ScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
     private int m_Points;
-    
     private bool m_GameOver = false;
 
-    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int score;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.score = score;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            score = data.score;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
